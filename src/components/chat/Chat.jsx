05949b57cc3ff0ed4346d97ff635ddcb03684c9e
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-22 19:55:14 -0400

    Chat Navbar

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import '../chat/Chat.jsx.css'
 
 const Chat = () => {
     return (
         <div className='chat'>
-            Chat
+            <div className="chatInfo">
+                <span>Jane</span>
+                <div className="chatIcons">
+                    <img src="https://raw.githubusercontent.com/safak/youtube2022/react-chat/src/img/cam.png" alt="" />
+                    <img src="https://raw.githubusercontent.com/safak/youtube2022/react-chat/src/img/add.png" alt="" />
+                    <img src="https://raw.githubusercontent.com/safak/youtube2022/react-chat/src/img/more.png" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
