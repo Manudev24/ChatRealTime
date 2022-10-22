ed87ee09127a973dd892cff39c2c50726477a561
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-22 18:59:05 -0400

    Navbar y Search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -5,7 +5,7 @@ const Search = () => {
     return (
         <div className="search">
             <div className="searchForm">
-                <input type="text"/>
+                <input type="text" placeholder="Find a User"/>
             </div>
             <div className="userchat">
                 <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
