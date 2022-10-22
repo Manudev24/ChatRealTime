436303c8c77326573ac289ac90fcdfe5fbe1dd80
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-21 23:01:19 -0400

    Navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import '../navbar/navbar.jsx.css'
+
+const navbar = () => {
+
+    return (
+        <div className='nav'>
+            <div className='nav-header'>
+                <div className='nav-title'>
+                    ChatRealTime
+                </div>
+            </div>
+            <div className='nav-body'>
+                <ul>
+                    <li className='nav-item'>
+                        <a href="#0">Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="https://github.com/Manudev24/ChatRealTime">Github</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+    
+}
+export default navbar
