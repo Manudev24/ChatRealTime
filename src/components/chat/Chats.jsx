99cbd56ac9352a4ef50775c19a9b101439e77588
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-10-22 20:54:30 -0400

    Update Chats.jsx

--- src/components/chat/Chats.jsx
+++ src/components/chat/Chats.jsx
@@ -5,28 +5,28 @@ const Chats = () => {
     return (
         <div className="chats">
             <div className="userchat">
-                <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
+                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
                 <div className="userChatInfo">
                     <span>Jhon</span>
                     <p>Hello</p>
                 </div>
             </div>
             <div className="userchat">
-                <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
+                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
                 <div className="userChatInfo">
                     <span>Jhon</span>
                     <p>Hello</p>
                 </div>
             </div>
             <div className="userchat">
-                <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
+                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
                 <div className="userChatInfo">
                     <span>Jhon</span>
                     <p>Hello</p>
                 </div>
             </div>
             <div className="userchat">
-                <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
+                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
                 <div className="userChatInfo">
                     <span>Jhon</span>
                     <p>Hello</p>
