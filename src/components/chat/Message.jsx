29f5442b7ec16c434bff8aa8873d91db99c71a70
Manudev24 <victor_estevez@ucne.edu.do>
2022-10-23 10:22:16 -0400

    First Migration to sass

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -2,8 +2,15 @@ import React from "react"
 
 const Message = () => {
     return (
-        <div className="message">
-            Message
+        <div className="message owner">
+            <div className="messageInfo">
+                <img src="https://static.vecteezy.com/system/resources/previews/002/399/634/original/v-letter-logo-business-template-icon-free-vector.jpg" />
+                <span>Just now</span>
+            </div>
+            <div className="messageContent">
+                <p>Hello</p>
+                {/* <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" /> */}
+            </div>
         </div>
     )
 }
