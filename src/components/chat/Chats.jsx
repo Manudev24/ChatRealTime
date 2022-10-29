aff6ccb1cec1e57fb556e7cb25ff4b160da4fe27
Manudev24 <victorestevez@gmail.com>
2022-10-29 16:27:19 -0400

    Search User

--- src/components/chat/Chats.jsx
+++ src/components/chat/Chats.jsx
@@ -3,34 +3,7 @@ import React from "react"
 const Chats = () => {
     return (
         <div className="chats">
-            <div className="userchat">
-                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>Jhon</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userchat">
-                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>Jhon</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userchat">
-                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>Jhon</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userchat">
-                <img src="https://i.pinimg.com/originals/03/32/0d/03320d5d2bcc9a44c41e7cd04a94b006.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>Jhon</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+
         </div>
     )
 }
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,20 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase"
+import { async } from "@firebase/util";
 
 const Search = () => {
+
+    const [username, setUsername] = useState("");
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState("");
+
+    const handleSearch = async () => {
+
+        const q = query(collection(db, "users"), where("displayName", "==", username));
+
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data())
+            });
+
+
+        } catch (err) {
+            setErr(true);
+        }
+    };
+
+    const handlekey = (e) => {
+        e.code === "Enter" && handleSearch();
+    }
+
     return (
         <div className="search">
             <div className="searchForm">
-                <input type="text" placeholder="Find a User" />
+                <input
+                    type="text"
+                    placeholder="Find a User"
+                    onKeyDown={handlekey}
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
             </div>
-            {/*<div className="userchat">
-                <img src="https://www.adobe.com/es/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" alt="" />
+            {err && <span>Without results</span>}
+            {user && (<div className="userchat">
+                <img src={user.photoURL} alt="" />
                 <div className="userChatInfo">
-                    <span>Jhon</span>
+                    <span>{user.displayName}</span>
                 </div>
-            </div>
-    */}
+            </div>)
+            }
         </div>
-    )
-}
-
+    );
+};
 export default Search
